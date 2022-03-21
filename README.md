b0713432c32964a62d3f9d17b1b2d19feb79f327
Your Name <marthaball@ufl.edu>
2022-03-20 20:51:29 -0400

    worked on css and added javascript

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # m5-hw8-ball-martha
 
-Issues: I did not have all the CSS I needed in my head. 
+Issues: I did not have all the CSS I needed in my head for borders
+I had issues with media query and making everything work the right way
 The javascript wasn't working at first but I found my typos
 
 
--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 // Create an Array of at least 3 losing messages
+let losingMessage = ["Sorry, you lose.", "Ouch! Not a winner!", "You lost this one"];
+console.log(losingMessage);
 
 // Create variables to count wins and losses
+let winScore = 0;
+let loseScore = 0;
+console.log("Your win score:", winScore);
+console.log("Your lose score:", loseScore);
+
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+let message = document.getElementById("message");
+let wins = document.getElementById("wins");
+let losses = document.getElementById("losses");
+console.log(message);
+console.log(wins);
+console.log(losses);
 
 // target all .box elements and attach a click event listener to each one using a loop
+var box = document.querySelectorAll('.box');
+for (var i = 0; i < box.length; i++) {
+  box[i].onclick = clickedBox
+}
+
 
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
+function clickedBox(event) {
+    clickedBox.textContent = event.target.textContent;
+    var numberOfBoxClicked = Number(event.target.textContent);
+    console.log(event.target)
+ 
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
+var randomNumberWinner = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumberWinner);
+
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
-
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+
+if (numberOfBoxClicked === randomNumberWinner) {
+    console.log("You won!");
+    message.textContent = "You won!";
+    winScore += 1;
+    wins.textContent = "Wins:" + winScore;
+    console.log("Your win score:", winScore);
+    } else {
+      console.log(losingMessage);
+    message.textContent = losingMessage[Math.floor(Math.random() * 3)];
+    loseScore += 1;
+    losses.textContent = "Losses:" + loseScore;
+    console.log("Your lose score:", loseScore);
+    }
+  }
+  
